ce2933e602aabc63a84be5691e5201167c5c6908
Jmar33 <josimarferreira922@yahoo.com.br>
2025-02-19 20:45:24 -0300

    feat: using dependency injection with directives

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -9,6 +9,7 @@ import { Directive, input } from "@angular/core";
 })
 export class SafeLinkDirective {
   queryParam = input('myapp', {alias: 'appSafeLink'})
+  private hostElmentRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
   constructor(){
     console.log('Safelink directive is active!')
@@ -18,8 +19,8 @@ export class SafeLinkDirective {
     const wantsToLeave = window.confirm("Do you want to leave the app?");
     
     if(wantsToLeave){
-      const address = (event.target as HTMLAnchorElement).href;
-      (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+      const address = this.hostElmentRef.nativeElement.href;
+      this.hostElmentRef.nativeElement.href = address + '?from=' + this.queryParam();
       return;
     }
 
